535ca0251a285eb008be29fb8a77ee166f56a6e0
megmeehey <megmeehey@gmail.com>
2017-08-05 20:46:32 +0300

    Added both Map tasks

---
+++ src/Map1.java
@@ -0,0 +1,93 @@
+import java.util.Map;
+
+public class Map1 {
+    public Map<String, String> mapBully(Map<String, String> map) {
+
+        if (map.containsKey("a")) {
+            String tmp = map.get("a");
+            map.put("a", "");
+            map.put("b", tmp);
+        }
+
+        return map;
+    }
+
+    public Map<String, String> mapShare(Map<String, String> map) {
+        if (map.containsKey("a")) {
+            map.put("b", map.get("a"));
+        }
+        map.remove("c");
+        return map;
+    }
+
+    public Map<String, String> mapAB(Map<String, String> map) {
+        if (map.containsKey("a") && map.containsKey("b")) {
+            map.put("ab", map.get("a") + map.get("b"));
+        }
+        return map;
+    }
+
+    public Map<String, String> topping1(Map<String, String> map) {
+        map.put("bread", "butter");
+        if (map.containsKey("ice cream")) {
+            map.put("ice cream", "cherry");
+        }
+        return map;
+    }
+
+    public Map<String, String> topping2(Map<String, String> map) {
+        // If the key "spinach" has a value, change that value to "nuts".
+        if (map.containsKey("ice cream")) {
+            map.put("yogurt", map.get("ice cream"));
+        }
+        if (map.containsKey("spinach")) {
+            map.put("spinach", "nuts");
+        }
+        return map;
+    }
+
+    public Map<String, String> topping3(Map<String, String> map) {
+        if (map.containsKey("potato")) {
+            map.put("fries", map.get("potato"));
+        }
+        if (map.containsKey("salad")) {
+            map.put("spinach", map.get("salad"));
+        }
+        return map;
+    }
+
+    public Map<String, String> mapAB2(Map<String, String> map) {
+        if (map.containsKey("a") && map.containsKey("b") && map.get("a").equals(map.get("b"))) {
+            map.remove("a");
+            map.remove("b");
+        }
+        return map;
+    }
+
+    public Map<String, String> mapAB3(Map<String, String> map) {
+        if (map.containsKey("a") ^ map.containsKey("b")) {
+            if (map.containsKey("a")) {
+                map.put("b", map.get("a"));
+            } else {
+                map.put("a", map.get("b"));
+            }
+        }
+        return map;
+    }
+
+    public Map<String, String> mapAB4(Map<String, String> map) {
+        if (map.containsKey("a") && map.containsKey("b")) {
+            if (map.get("a").length() != map.get("b").length()) {
+                if (map.get("a").length() > map.get("b").length()) {
+                    map.put("c", map.get("a"));
+                } else {
+                    map.put("c", map.get("b"));
+                }
+            } else {
+                map.put("a", "");
+                map.put("b", "");
+            }
+        }
+        return map;
+    }
+}
---
+++ src/Map2.java
@@ -0,0 +1,126 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Map2 {
+    public Map<String, Integer> word0(String[] strings) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String s : strings) {
+            map.put(s, 0);
+        }
+        return map;
+    }
+
+    public Map<String, Integer> wordLen(String[] strings) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String s : strings) {
+            map.put(s, s.length());
+        }
+        return map;
+    }
+
+    public Map<String, String> pairs(String[] strings) {
+        Map<String, String> map = new HashMap<>();
+        for (String s : strings) {
+            String first = "" + s.charAt(0);
+            String last = "" + s.charAt(s.length() - 1);
+            map.put(first, last);
+        }
+        return map;
+    }
+
+    public Map<String, Integer> wordCount(String[] strings) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String s : strings) {
+            if (map.containsKey(s)) {
+                map.put(s, map.get(s) + 1);
+            } else {
+                map.put(s, 1);
+            }
+        }
+        return map;
+    }
+
+    public Map<String, String> firstChar(String[] strings) {
+        Map<String, String> map = new HashMap<>();
+        for (String s : strings) {
+            String first = "" + s.charAt(0);
+            if (map.containsKey(first)) {
+                map.put(first, map.get(first) + s);
+            } else {
+                map.put(first, s);
+            }
+        }
+        return map;
+    }
+
+    public String wordAppend(String[] strings) {
+        Map<String, Integer> map = new HashMap<>();
+        StringBuilder result = new StringBuilder();
+        for (String s : strings) {
+            if (map.containsKey(s)) {
+                map.put(s, map.get(s) + 1);
+            } else {
+                map.put(s, 1);
+            }
+            if (map.get(s) % 2 == 0) {
+                result.append(s);
+            }
+        }
+        return result.toString();
+    }
+
+    public Map<String, Boolean> wordMultiple(String[] strings) {
+        Map<String, Integer> counts = new HashMap<String, Integer>();
+        Map<String, Boolean> result = new HashMap<String, Boolean>();
+        for (int i = 0; i < strings.length; i++) {
+            String key = strings[i];
+            if (counts.containsKey(key)) {
+                int val = counts.get(key);
+                val++;
+                counts.put(key, val);
+            } else {
+                counts.put(key, 1);
+            }
+            result.put(key, counts.get(key) >= 2);
+        }
+        return result;
+    }
+
+    public String[] allSwap(String[] strings) {
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < strings.length; i++) {
+            String key = String.valueOf(strings[i].charAt(0));
+            if (map.containsKey(key)) {
+                int pos = map.get(key);
+                String tmp = strings[pos];
+                strings[pos] = strings[i];
+                strings[i] = tmp;
+                map.remove(key);
+            } else {
+                map.put(key, i);
+            }
+        }
+        return strings;
+    }
+
+    public String[] firstSwap(String[] strings) {
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < strings.length; i++) {
+            String key = String.valueOf(strings[i].charAt(0));
+            if (map.containsKey(key)) {
+                int value = map.get(key);
+                if (value == -1) {
+                    continue;
+                }
+                int pos = map.get(key);
+                String tmp = strings[pos];
+                strings[pos] = strings[i];
+                strings[i] = tmp;
+                map.put(key, -1);
+            } else {
+                map.put(key, i);
+            }
+        }
+        return strings;
+    }
+}
