a5ac85fc59971bf5f121fe3f30306fa1ff4b7db7
megmeehey <megmeehey@gmail.com>
2017-08-05 12:29:23 +0300

    Last tasks from Recursion 2 Finally...

--- src/Recursion2.java
+++ src/Recursion2.java
@@ -39,4 +39,68 @@ public class Recursion2 {
         return groupSum5(start + 1, nums, target - nums[start])
                 || groupSum5(start + 1, nums, target);
     }
+
+    public boolean groupSumClump(int start, int[] nums, int target) {
+        if (start >= nums.length) {
+            return target == 0;
+        }
+
+        int sum = nums[start];
+        int count = 1;
+        // that one loop, that we use to find the extent
+        for (int i = start + 1; i < nums.length; i++) {
+            if (nums[i] == nums[start]) {
+                sum += nums[i];
+                count++;
+            }
+        }
+        return groupSumClump(start + count, nums, target - sum)
+                || groupSumClump(start + count, nums, target);
+    }
+
+    public boolean splitArray(int[] nums) {
+        // Not enough arguments, so i used another method
+        return sArray(nums, 0, 0, 0);
+    }
+
+    private boolean sArray(int[] nums, int start, int sum1, int sum2) {
+        if (start >= nums.length) {
+            return sum1 == sum2;
+        }
+        return sArray(nums, start + 1, sum1 + nums[start], sum2)
+                || sArray(nums, start + 1, sum1, sum2 + nums[start]);
+    }
+
+    public boolean splitOdd10(int[] nums) {
+        return helper(0, nums, 0, 0);
+    }
+
+    private boolean helper(int start, int[] nums, int sum1, int sum2) {
+        if (start >= nums.length) {
+            return (sum1 % 10 == 0 && sum2 % 2 == 1) || (sum1 % 2 == 1
+                    && sum2 % 10 == 0);
+        }
+        return helper(start + 1, nums, sum1 + nums[start], sum2)
+                || helper(start + 1, nums, sum1, sum2 + nums[start]);
+    }
+
+    public boolean split53(int[] nums) {
+        return s53(0, nums, 0, 0);
+    }
+
+    private boolean s53(int start, int[] nums, int sum1, int sum2) {
+        if (start >= nums.length) {
+            return sum1 == sum2;
+        }
+        if (nums[start] % 5 == 0) {
+            return s53(start + 1, nums, sum1 + nums[start], sum2);
+        }
+        // fizzbuzz
+        if (nums[start] % 3 == 0) {
+            return s53(start + 1, nums, sum1, sum2 + nums[start]);
+        }
+
+        return s53(start + 1, nums, sum1 + nums[start], sum2)
+                || s53(start + 1, nums, sum1, sum2 + nums[start]);
+    }
 }
